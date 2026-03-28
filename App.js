10e0c430e2bab895578c4a6420251438d531bad9
DobariyaMayank1 <dmayank2115@gmail.com>
2026-03-29 02:34:11 +0530

    Implement Auto login in App component

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, ActivityIndicator } from "react-native";
 
 import BottomTabs from "./src/navigation/BottomTabs";
 import LoginScreen from "./src/screens/LoginScreen";
@@ -7,15 +10,44 @@ import LoginScreen from "./src/screens/LoginScreen";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      setIsLoggedIn(true);
+    }
+
+    setLoading(false);
+  };
+
+  // ⏳ While checking
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen
-          name="Main"
-          component={BottomTabs}
-          options={{ headerShown: false }}
-        />
+        {!isLoggedIn ? (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        ) : (
+          <Stack.Screen
+            name="Main"
+            component={BottomTabs}
+            options={{ headerShown: false }}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
